e2fc297fc4e2b1c708609f0d3b38f3c4d3046e9b
GodeGamleJacob <89967283+GodeGamleJacob@users.noreply.github.com>
2021-09-08 14:11:17 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {                             //
+
+    public static void main(String[] args) {    //1
+        System.out.print("No, I ");             //2
+        zoop();                                 //3
+        System.out.print("I ");                 //24
+        baffle();                               //25
+    }                                           //34
+                                                //
+    public static void zoop() {                 //4 + 22
+        baffle();                               //5
+        System.out.print("You wugga ");         //13
+        baffle();                               //14
+    }                                           // + 23 + 33
+                                                //
+    public static void baffle() {               //6 + 15 + 26
+        System.out.print("wug");                //7 + 16 + 27
+        ping();                                 //8 + 17 + 28
+    }                                           //12 + 21+ 32
+    public static void ping() {                 //9 + 18 + 29
+        System.out.println(".");                //10 + 19 + 30
+    }                                           //11 + 20 + 31
+}                                               //35
